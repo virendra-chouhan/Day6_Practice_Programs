842364ba12013f6cd0656122bca95eee6cc6fb78
virendra <virendrachouhan1494@gmail.com>
2022-08-03 11:25:18 +0530

    Coupon_Number

--- Day6_Practice_Problem/Day6_Practice_Problem/Program.cs
+++ Day6_Practice_Problem/Day6_Practice_Problem/Program.cs
@@ -6,21 +6,18 @@ namespace Day6_Practice_Problem
     {
         static void Main(string[] args)
         {
-            int n, i, m = 0, flag = 0;
-            Console.Write("Enter the Number to check Prime: ");
-            n = int.Parse(Console.ReadLine());
-            m = n / 2;
-            for (i = 2; i <= m; i++)
+            Random random = new Random();
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            var stringChars = new char[15];
+
+            for (int i = 0; i < stringChars.Length; i++)
             {
-                if (n % i == 0)
-                {
-                    Console.Write("Number is not Prime.");
-                    flag = 1;
-                    break;
-                }
+                stringChars[i] = chars[random.Next(chars.Length)];
+                Console.WriteLine(" Your Coupon Number is: " + stringChars[i]);
+
             }
-            if (flag == 0)
-                Console.Write("Number is Prime.");
         }
 
     }
